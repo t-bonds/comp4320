1ab662b78a89ee62c27d804fdd706dcbaaf88ccc
Tanner Bonds <tjbonds98@gmail.com>
2019-09-06 08:08:54 -0500

    Changed from run arguments to prompting user.

--- assignment1/myFirstTCPClient.java
+++ assignment1/myFirstTCPClient.java
@@ -1,18 +1,30 @@
 import java.net.*;  // for Socket
 import java.io.*;   // for IOException and Input/OutputStream
+import java.util.Scanner; // for Scanner
 
 public class myFirstTCPClient {
 
 public static void main(String[] args) throws IOException {
 
-        if ((args.length < 2) || (args.length > 3)) // Test for correct # of args
+        if ((args.length < 1) || (args.length > 2)) // Test for correct # of args
                 throw new IllegalArgumentException("Parameter(s): <Server> <Word> [<Port>]");
 
-        String server = args[0];   // Server name or IP address
-        // Convert input String to bytes using the default character encoding
-        byte[] byteBuffer = args[1].getBytes();
+        String server = args[0]; // Server name or IP address
 
-        int servPort = (args.length == 3) ? Integer.parseInt(args[2]) : 7;
+
+
+        int servPort = (args.length == 2) ? Integer.parseInt(args[1]) : 7;
+
+        System.out.print("Please enter a message shorter than 128 characters: ");
+        Scanner scan = new Scanner(System.in);
+        String firstAddress = scan.next();
+        if (firstAddress.length() <=128) {
+                System.out.println("Your message is: "+ firstAddress);
+        }
+        else {
+          throw new IllegalArgumentException("Your message must be shorter than 128 characters.");
+        }
+
 
         // Create socket that is connected to server on specified port
         Socket socket = new Socket(server, servPort);
@@ -21,19 +33,19 @@ public static void main(String[] args) throws IOException {
         InputStream in = socket.getInputStream();
         OutputStream out = socket.getOutputStream();
 
-        out.write(byteBuffer); // Send the encoded string to the server
+        out.write(firstAddress.getBytes()); // Send the encoded string to the server
 
         // Receive the same string back from the server
         int totalBytesRcvd = 0; // Total bytes received so far
-        int bytesRcvd;       // Bytes received in last read
-        while (totalBytesRcvd < byteBuffer.length) {
-                if ((bytesRcvd = in.read(byteBuffer, totalBytesRcvd,
-                                         byteBuffer.length - totalBytesRcvd)) == -1)
+        int bytesRcvd;     // Bytes received in last read
+        while (totalBytesRcvd < firstAddress.getBytes().length) {
+                if ((bytesRcvd = in.read(firstAddress.getBytes(), totalBytesRcvd,
+                                         firstAddress.getBytes().length - totalBytesRcvd)) == -1)
                         throw new SocketException("Connection close prematurely");
                 totalBytesRcvd += bytesRcvd;
         }
 
-        System.out.println("Received: " + new String(byteBuffer));
+        System.out.println("Received: " + new String(firstAddress.getBytes()));
 
         socket.close(); // Close the socket and its streams
 }
