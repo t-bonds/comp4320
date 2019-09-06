785cec288c73acbe681e258385a269834be6c1ad
Tanner Bonds <tjbonds98@gmail.com>
2019-09-06 08:38:50 -0500

    Beautified code

--- assignment1/myFirstTCPClient.java
+++ assignment1/myFirstTCPClient.java
@@ -17,44 +17,44 @@ public static void main(String[] args) throws IOException {
 
         for (;;) {
 
-        System.out.print("Please enter a message shorter than 128 characters: ");
-        Scanner scan = new Scanner(System.in);
-        String firstAddress = scan.nextLine(); //nextLine allows for spaces inside the string
-        if (firstAddress.length() <= 128) {
-                System.out.println("Your message is: "+ firstAddress);
+                System.out.print("Please enter a message shorter than 128 characters: ");
+                Scanner scan = new Scanner(System.in);
+                String firstAddress = scan.nextLine(); //nextLine allows for spaces inside the string
+                if (firstAddress.length() <= 128) {
+                        System.out.println("Your message is: "+ firstAddress);
+                }
+                else {
+                        throw new IllegalArgumentException("Your message must be shorter than 128 characters.");
+                }
+
+
+                // Create socket that is connected to server on specified port
+                Socket socket = new Socket(server, servPort);
+                System.out.println("Connected to server...sending echo string");
+
+                InputStream in = socket.getInputStream();
+                OutputStream out = socket.getOutputStream();
+
+                out.write(firstAddress.getBytes()); // Send the encoded string to the server
+
+                // Receive the same string back from the server
+                int totalBytesRcvd = 0; // Total bytes received so far
+                int bytesRcvd; // Bytes received in last read
+
+                //TODO MEASURE DURATION BETWEEN WHEN MESSAGE WAS SENT AND RECEIVED
+                //TODO PRINT TIME MESSAGE RECEIVED
+                //TODO COLLECT ROUND TRIP TIME
+
+                while (totalBytesRcvd < firstAddress.getBytes().length) {
+                        if ((bytesRcvd = in.read(firstAddress.getBytes(), totalBytesRcvd,
+                                                 firstAddress.getBytes().length - totalBytesRcvd)) == -1)
+                                throw new SocketException("Connection close prematurely");
+                        totalBytesRcvd += bytesRcvd;
+                }
+
+                System.out.println("Received: " + new String(firstAddress.getBytes()));
+
+                socket.close(); // Close the socket and its streams
         }
-        else {
-          throw new IllegalArgumentException("Your message must be shorter than 128 characters.");
-        }
-
-
-        // Create socket that is connected to server on specified port
-        Socket socket = new Socket(server, servPort);
-        System.out.println("Connected to server...sending echo string");
-
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
-
-        out.write(firstAddress.getBytes()); // Send the encoded string to the server
-
-        // Receive the same string back from the server
-        int totalBytesRcvd = 0; // Total bytes received so far
-        int bytesRcvd;     // Bytes received in last read
-
-        //TODO MEASURE DURATION BETWEEN WHEN MESSAGE WAS SENT AND RECEIVED
-        //TODO PRINT TIME MESSAGE RECEIVED
-        //TODO COLLECT ROUND TRIP TIME
-
-        while (totalBytesRcvd < firstAddress.getBytes().length) {
-                if ((bytesRcvd = in.read(firstAddress.getBytes(), totalBytesRcvd,
-                                         firstAddress.getBytes().length - totalBytesRcvd)) == -1)
-                        throw new SocketException("Connection close prematurely");
-                totalBytesRcvd += bytesRcvd;
-        }
-
-        System.out.println("Received: " + new String(firstAddress.getBytes()));
-
-        socket.close(); // Close the socket and its streams
-      }
 }
 }
