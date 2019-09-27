87315297949a76e36e635a6ea30031bcf8c0598a
Tanner Bonds <tjbonds98@gmail.com>
2019-09-27 09:08:25 -0500

    Update

--- assignment2/Request/ClientUDP.java
+++ assignment2/Request/ClientUDP.java
@@ -17,47 +17,54 @@ public static void main(String args[]) throws Exception {
         int TML, opCode, op1;
         int operands = 1;
         int op2 = 0;
+        for (;;) {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Please Enter The Following Values:");
+                System.out.print("\tOperand Type: \n\t(\"0\" = +, \"1\" = -, \"2\" = *, \"3\" = /, \"4\" = >>, \"5\" = <<, \"6\" = ~, \"exit\"): ");
+                if (scan.nextLine().equals("exit")) {
+                        break;
+                }
+                opCode = Integer.parseInt(scan.nextLine());
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Please Enter The Following Values:");
-        System.out.print("\tOperand Type: \n\t(0 = +, 1 = -, 2 = *, 3 = /, 4 = >>, 5 = <<, 6 = ~): ");
-        opCode = Integer.parseInt(scan.nextLine());
+                System.out.print("\n\tOperand 1: ");
+                op1 = Integer.parseInt(scan.nextLine());
+                //TODO CONVERT MESSAGE TO BINARY
+                //TODO CALCULATE TML FOR ~
 
-        System.out.print("\n\tOperand 1: ");
-        op1 = Integer.parseInt(scan.nextLine());
 
-        //TODO CALCULATE TML FOR ~
-        //TML =
 
-        if (opCode <=5) {
+                if (opCode <=5) {
 
-                System.out.print("\n\tOperand 2: ");
-                op2 = Integer.parseInt(scan.nextLine());
-                operands = 2;
-                //TODO CALCULATE TML FOR ALL OTHER OPCODE
-                //TML =
+                        System.out.print("\n\tOperand 2: ");
+                        op2 = Integer.parseInt(scan.nextLine());
+                        operands = 2;
+                        //TODO CONVERT MESSAGE TO BINARY
+                        //TODO CALCULATE TML FOR ALL OTHER OPCODE
+                        //TML =
 
+                }
+                //TODO CREATE RANDOM REQUEST ID
+                int id = 1;
+
+
+                Request request = new Request(TML, ID, opCode, operands, op1, op2);
+
+                DatagramSocket sock = new DatagramSocket(); // UDP socket for sending
+
+
+                // Use the encoding scheme given on the command line (args[2])
+                RequestEncoder encoder = (args.length == 3 ?
+                                          new RequestEncoderBin(args[2]) :
+                                          new RequestEncoderBin());
+
+
+                byte[] codedRequest = encoder.encode(request); // Encode request
+
+                DatagramPacket message = new DatagramPacket(codedRequest, codedRequest.length,
+                                                            destAddr, destPort);
+                sock.send(message);
+
+                sock.close();
         }
-        //TODO CREATE ID
-
-
-        Request request = new Request(TML, ID, opCode, operands, op1, op2);
-
-        DatagramSocket sock = new DatagramSocket(); // UDP socket for sending
-
-
-        // Use the encoding scheme given on the command line (args[2])
-        RequestEncoder encoder = (args.length == 3 ?
-                                  new RequestEncoderBin(args[2]) :
-                                  new RequestEncoderBin());
-
-
-        byte[] codedRequest = encoder.encode(request); // Encode request
-
-        DatagramPacket message = new DatagramPacket(codedRequest, codedRequest.length,
-                                                    destAddr, destPort);
-        sock.send(message);
-
-        sock.close();
 }
 }
