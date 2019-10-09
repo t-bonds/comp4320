912bf6cfa7b7fb811b4ef1910b662e62af837a27
Tanner Bonds <tjbonds98@gmail.com>
2019-10-09 09:10:13 -0500

    Update

--- assignment3/ClientTCP.java
+++ assignment3/ClientTCP.java
@@ -2,18 +2,18 @@ import java.net.*;  // for DatagramSocket, DatagramPacket, and InetAddress
 import java.io.*;   // for IOException
 import java.util.Scanner; // for Scanner
 
-public class ClientUDP {
+public class ClientTCP {
 
    public static void main(String args[]) throws Exception {
-   
-      if (args.length != 2 && args.length != 3)  // Test for correct # of args        
+
+      if (args.length != 2 && args.length != 3)  // Test for correct # of args
          throw new IllegalArgumentException("Parameter(s): <Destination>" +
                " <Port> [<encoding]");
-        
-        
+
+
       InetAddress destAddr = InetAddress.getByName(args[0]);  // Destination address
       int destPort = Integer.parseInt(args[1]) + TCPRequestBinConst.GROUP_NUMBER;  // Destination port
-   
+
       for(;;) {
          Byte TML = 6;
          Byte ID = 1;
@@ -21,26 +21,26 @@ public class ClientUDP {
          Byte opCode;
          Short op1 = 0;
          Short op2 = 0;
-      
+
          Scanner scan = new Scanner(System.in);
          System.out.println("Please Enter The Following Values:");
          System.out.print("\tOperand Type - \n\t\"0\" = +, \n\t\"1\" = -, \n\t\"2\" = *, \n\t\"3\" = /, \n\t\"4\" = >>, \n\t\"5\" = <<, \n\t\"6\" = ~, \n\t\"exit\" = Terminate \n\tEnter Selection: ");
-      
+
          String opString = scan.nextLine();
-      
+
          if (opString.equals("exit")) {
             break;
          }
          int opCodeInt = Integer.parseInt(opString);
          opCode = (byte) opCodeInt;
-      
-      
-                  
+
+
+
          System.out.print("\n\tOperand 1: ");
          int op1Int = Integer.parseInt(scan.nextLine());
          op1 = (short) op1Int;
-      
-      
+
+
          if (opCode <= 5) {
             System.out.print("\n\tOperand 2: ");
             int op2Int = Integer.parseInt(scan.nextLine());
@@ -48,54 +48,56 @@ public class ClientUDP {
             TML = (byte) 8;
             operands = (byte) 2;
          }
-      
+
          TCPRequest Request = new TCPRequest(TML, ID, opCode, operands, op1, op2);
-         
-         DatagramSocket sock = new DatagramSocket(); // UDP socket for sending
-         
-         
+
+         Socket sock = new Socket(destAddr, destPort); // TCP socket for sending
+
+
          // Use the encoding scheme given on the command line (args[2])
          TCPRequestEncoder encoder = (args.length == 3 ?
               new TCPRequestEncoderBin(args[2]) :
               new TCPRequestEncoderBin());
-         
+
          byte[] codedRequest = encoder.encode(Request); // Encode Request
-         
-         DatagramPacket message = new DatagramPacket(codedRequest, codedRequest.length, 
-                destAddr, destPort);
-      
-         System.out.println("Message length: " + message.getLength());
+
+
+         System.out.println("Message length: " + codedRequest.length);
          System.out.print("Hex String: ");
-         byte[] requestBuffer = message.getData();
+         //byte[] requestBuffer = message.getData();
          for (int i = 0; i < Request.TML; i++) {
-            System.out.format("\t0x%x\n", requestBuffer[i]);
+            System.out.format("\t0x%x\n", codedRequest[i]);
          }
-                  
-         sock.send(message);
-         long start = System.nanoTime();
-      
-         sock.receive(message);
-         long finish = System.nanoTime();
-         long elapsed = finish - start;
-      
-         TCPRequestDecoder decoder = (args.length == 2 ?   // Which encoding              
+         //InputStream in = sock.getInputStream();
+         OutputStream out = sock.getOutputStream();
+
+
+         TCPRequestDecoder decoder = (args.length == 2 ?   // Which encoding
               new TCPRequestDecoderBin(args[1]) :
               new TCPRequestDecoderBin() );
-      
-         TCPResponse Response = decoder.decodeResponse(message);
-      
+
+         out.write(codedRequest);
+         long start = System.nanoTime();
+
+
+         TCPResponse Response = decoder.decodeResponse(sock.getInputStream());
+         long finish = System.nanoTime();
+         long elapsed = finish - start;
+
+
          //print stuff
          System.out.print("Hex String: ");
-         byte[] responseBuffer = message.getData();
+         //TODO FIX!!!
+         //byte[] codedResponse = decoder.decode(Response);
          for (int i = 0; i < Response.TML; i++) {
-            System.out.format("\t0x%x\n", responseBuffer[i]);
+            System.out.format("\t0x%x\n", codedResponse[i]);
          }
-      
+
          System.out.println("\n" + Response);
          System.out.println("Time Elapsed: " + elapsed / 1000000.0 + "ms\n");
       }
-   
-      
+
+
       //sock.close();
    }
 }
--- assignment3/ServerTCP.java
+++ assignment3/ServerTCP.java
@@ -1,67 +1,67 @@
 import java.net.*;  // for DatagramSocket and DatagramPacket
 import java.io.*;   // for IOException
 
-public class ServerUDP {
+public class ServerTCP {
 
    public static void main(String[] args) throws Exception {
-   
-      if (args.length != 1 && args.length != 2)  // Test for correct # of args        
+
+      if (args.length != 1 && args.length != 2)  // Test for correct # of args
          throw new IllegalArgumentException("Parameter(s): <Port> [<encoding>]");
-      
+
       int port = Integer.parseInt(args[0]) + TCPRequestBinConst.GROUP_NUMBER;   // Receiving Port
-      
-      DatagramSocket sock = new DatagramSocket(port);  // UDP socket for receiving      
+
+      DatagramSocket sock = new DatagramSocket(port);  // UDP socket for receiving
       DatagramPacket packet = new DatagramPacket(new byte[1024],1024);
-   
+
       System.out.println("Server Is Active. Enter \"Control + C\" to Terminate.\n");
-   
-      for (;;) {  
+
+      for (;;) {
          System.out.println("Server is Idle. Awaiting Packets.\n");
-      
+
          sock.receive(packet);
-      
-      
+
+
          System.out.println("Message length: " + packet.getLength());
          System.out.print("\nHex String: ");
          byte[] receiveBuffer = packet.getData();
          for (int i = 0; i < 8; i++) {
             System.out.format("\t0x%x\n", receiveBuffer[i]);
          }
-         
-      
-        
-         TCPRequestDecoder decoder = (args.length == 2 ?   // Which encoding              
+
+
+
+         TCPRequestDecoder decoder = (args.length == 2 ?   // Which encoding
             new TCPRequestDecoderBin(args[1]) :
             new TCPRequestDecoderBin() );
-      
+
          TCPRequest Request = decoder.decodeRequest(packet);
-      
-      
+
+
       // Print receive confirmation
          System.out.println("Received Binary-Encoded Request");
-      
+
          System.out.print("\nHex String: ");
          byte[] byteBuffer = packet.getData();
          for (int i = 0; i < Request.TML; i++) {
             System.out.format("\t0x%x\n", byteBuffer[i]);
          }
-      
+
          System.out.println();
          System.out.println(Request);
-      
+
          byte error = 0;
          if (packet.getLength() != Request.TML) {
             error = (byte) 127;
          }
-      
+
          int result = calculate(Request.opCode, Request.op1, Request.op2);
-      
+
          TCPResponse Response = new TCPResponse(Request.TML, Request.ID, error, result);
-      
+
          TCPRequestEncoder encoder = (args.length == 3 ?
             new TCPRequestEncoderBin(args[2]) :
             new TCPRequestEncoderBin());
-      
+
          byte[] codedResponse = encoder.encode(Response); // Encode Request
          packet.setData(codedResponse);
          packet.setLength(codedResponse.length);
