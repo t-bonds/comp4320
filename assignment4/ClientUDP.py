a70886f86cc2733ded3dc2f79bc3090cc3bcca5a
Tanner Bonds <tjbonds98@gmail.com>
2019-11-07 20:21:54 -0600

    LIST INDICES MUST BE INTEGERS

--- assignment4/ClientUDP.py
+++ assignment4/ClientUDP.py
@@ -3,7 +3,7 @@ import sys
 import time
 
 GROUP_NUMBER = 24
-DEFAULT_ENCODING = "ISO-8859-1"
+DEFAULT_ENCODING = 'ISO-8859-1'
 
 ipAddress = sys.argv[1]
 port = int(sys.argv[2])
@@ -14,42 +14,42 @@ if len(sys.argv) != 2 and len(sys.argv) != 3:
 
 serverAddress = (ipAddress, gPort)
 
-ID = bytes(1)
-opCode = bytes(1)
+ID = 1
+opCode = 1
 op1 = 0.0
 op2 = 0.0
 exitStr = 'exit'
 
 while (True):
-    TML = bytes(6)
-    operands = bytes(1)
+    TML = 6
+    operands = 1
 
     print("Please Enter The Following Values:\n")
     opString = input('\tOperand Type - \n\t\"0\" = + \n\t\"1\" = - \n\t\"2\" = * \n\t\"3\" = / \n\t\"4\" = >>  \n\t\"5\" = << \n\t\"6\" = ~ \n\t\"exit\" = Terminate \n\tEnter Selection: ')
-    if opString == exitStr:
+    if str(opString) in "exit":
         sys.exit(0)
-    opCodeInt = int(opString)
-    opCode = bytes(opCodeInt)
 
-    op1Int = int(input("\n\tOperand 1: "))
-    op1 = float(op1Int)
+    opCode = int(opString)
+    op1 = float(input("\n\tOperand 1: "))
 
     if opCode <= 5:
-        op2Int = int(input("\n\tOperand 2: "))
-        op2 = float(op2Int)
-        TML = bytes(8)
-        operands = bytes(2)
+        op2 = float(input("\n\tOperand 2: "))
+        TML = 8
+        operands = 2
 
-    request = Request(TML, ID, opCode, operands, op1, op2)
+    requestStr = "TML: %d\nRequestID: %d\nOp Code: %d\nOperands: %d\nOperand 1: %d\nOperand 2: %d\n" % (
+        TML, ID, opCode, operands, op1, op2)
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    codedRequest = bytes(request.encode(DEFAULT_ENCODING))
+    request = [TML, ID, opCode, operands, op1, op2]
+
+    codedRequest = [str(x).encode(DEFAULT_ENCODING) for x in request]
 
     print('\n\nMessage Length: %d' % len(codedRequest))
     print('\n\nRequest Hex String: \n')
-    buffer = bytearray(codedRequest)
+    # buffer = bytearray(codedRequest)
     for i in codedRequest:
-        print('\t0x%d\n' % buffer[i])
+        print('\t0x%s\n' % int(codedRequest[i]))    #TODO LIST INDICES MUST BE INTEGERS, NOT STR
     send = sock.sendto(codedRequest, serverAddress)
     start = time.time()
 
@@ -57,7 +57,7 @@ while (True):
     finish = time.time()
     elapsed = start - finish
 
-    response = recv.decode(DEFAULT_ENCODING)
+    response = [str(x).decode(DEFAULT_ENCODING) for x in recv]
     print('\n\nMessage Length: %d' % len(recv))
     print('\n\nResponse Hex String: \n')
     byteBuffer = bytearray(recv)
@@ -68,9 +68,3 @@ while (True):
     print('\n\nTime ELapsed: %dms\n' % final)
     ID = ID + 1
 sock.close()
-
-
-def Request():
-    value = "TML: %d\nRequestID: %d\nOp Code: %d\nOperands: %d\nOperand 1: %d\nOperand 2: %d\n" % (
-        TML, ID, opCode, operands, op1, op2)
-    return value
